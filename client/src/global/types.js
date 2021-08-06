eb1a932264444f55f0b788a46c49f46fac2c48c3
Anant <anantarnav007@gmail.com>
2021-08-06 12:56:57 +0530

    added new page

--- client/src/global/types.js
+++ client/src/global/types.js
@@ -3,7 +3,7 @@ export const REMOVE_ALERT = "REMOVE_ALERT";
 export const ON_CSS_CODE_ADDED = "ON_CSS_CODE_ADDED";
 export const ON_JS_CODE_ADDED = "ON_JS_CODE_ADDED";
 export const CONVERT_CLICK = "CONVERT_CLICK";
-export const SAMPLE_CSS = `/* Developed by Anant S Awasthy */
+export const SAMPLE_CSS = `/* Developed by Arnav S Awasthy */
 .main {
   border: 10px solid violet;
   display: grid;
